f80a545d29478b0cb35b3bb94efb617628a29035
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2022-07-02 18:09:04 +0530

    created ExpenseDetails

--- src/App.js
+++ src/App.js
@@ -51,21 +51,20 @@ function App() {
         date={expenses[0].date}
         locationOfExpenditure={expenses[0].locationOfExpenditure}
       />
-      ;
+
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
         locationOfExpenditure={expenses[1].locationOfExpenditure}
       />
-      ;
+
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
         locationOfExpenditure={expenses[2].locationOfExpenditure}
       />
-      ;
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,13 @@
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
-      <h4>{props.locationOfExpenditure}</h4>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
-      </div>
+      <ExpenseDate date={props.date} />
+      {/* <h4>{props.locationOfExpenditure}</h4> */}
+      <ExpenseDetails amount={props.amount} title={props.title} />
     </div>
   );
 }
