a3e4cbeae865b084dae7d417a6f69f4a4df12392
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2022-07-04 19:28:41 +0530

    delete btn

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,20 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    const el = document.getElementById(props.id);
+    el.remove();
+  };
   return (
-    <Card className="expense-item">
+    <Card className="expense-item" id={props.id}>
       <ExpenseDate date={props.date} />
-      <ExpenseDetails amount={props.amount} title={props.title} />
+      <ExpenseDetails amount={props.amount} title={title} />
+      <button onClick={clickHandler}>Delete</button>
     </Card>
   );
 }
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,7 +2,11 @@ import "./Card.css";
 
 function Card(props) {
   const classes = "card " + props.className;
-  return <div className={classes}>{props.children}</div>;
+  return (
+    <div className={classes} id={props.id}>
+      {props.children}
+    </div>
+  );
 }
 
 export default Card;
