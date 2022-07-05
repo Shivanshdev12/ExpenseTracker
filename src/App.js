79ccbbd4ed09996c887c78ceecb5f081ffccdcfb
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2022-07-06 01:56:21 +0530

    ExpenseForm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -35,6 +36,7 @@ function App() {
   return (
     <div>
       <h1>Expense Tracker</h1>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
